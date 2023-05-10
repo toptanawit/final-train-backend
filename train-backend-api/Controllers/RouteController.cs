a539dbd288b1fcf0ccf422223967963d9865fc7a
toptanawit <tanavitptop@gmail.com>
2023-05-10 16:22:35 +0700

    bfs

--- train-backend-api/Controllers/RouteController.cs
+++ train-backend-api/Controllers/RouteController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -120,6 +122,97 @@ namespace TrainSystem.Controller
             }
 
             return new JsonResult("Delete Successfully");
+        }
+
+        // bfs
+
+        public List<string> stations = new List<string>();
+
+        public List<string[]> routes = new List<string[]> {
+            new string[] { "e12", "e4" }
+        };
+
+        public Dictionary<string, List<String>> adjacencyList = new Dictionary<String, List<String>>();
+
+        [HttpGet("{start}-{end}")]
+        public JsonResult GetAllPossibleRoutes(string start, string end)
+        {
+            string query = @"select * from stations";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TrainAppCon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+
+                    foreach (DataRow data in table.Rows)
+                    {
+                        stations.Add(data["station_id"].ToString());
+                    }
+
+                    void AddNode(string station)
+                    {
+                        adjacencyList.Add(station, new List<string>());
+                    }
+
+                    void AddEdge(string origin, string destination)
+                    {
+                        adjacencyList[origin].Add(destination);
+                        adjacencyList[destination].Add(origin);
+                    }
+
+                    foreach (string station in stations)
+                    {
+                        AddNode(station);
+                    }
+
+                    foreach (string[] route in routes)
+                    {
+                        AddEdge(route[0], route[1]);
+                    }
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            JsonResult BFS(string start, string end)
+            {
+                var visited = new HashSet<string>();
+                var queue = new Queue<string>();
+                queue.Enqueue(start);
+
+                while (queue.Count > 0)
+                {
+                    var station = queue.Dequeue();
+                    var destinations = adjacencyList[station];
+
+                    foreach (var destination in destinations)
+                    {
+                        if (destination == end)
+                        {
+                            visited.Add(destination);
+                            return new JsonResult(visited);
+                        }
+                        if (!visited.Contains(destination))
+                        {
+                            visited.Add(destination);
+                            queue.Enqueue(destination);
+                        }
+                    }
+                }
+
+                return new JsonResult(visited);
+            }
+
+            JsonResult result = BFS(start, end);
+
+            return result;
         }
     }
 }
